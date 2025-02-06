cc17efbc5d4471145799e0edb1337100acf06b30
varshitha101 <95077762+varshitha101@users.noreply.github.com>
2025-02-06 17:38:49 +0530

    Updated with version 1.5.4

--- index.js
+++ index.js
@@ -3434,8 +3434,8 @@ function saveToFirebase(data) {
 
     const dueDate = new Date();
     // dueDate.setMonth(dueDate.getMonth() + 6);  // Add 6 months to the current date
-    // dueDate.setMinutes(dueDate.getMinutes() + 3);  // Add 10 days to the current date
-    dueDate.setMinutes(dueDate.getMinutes() + 10 * 24 * 60);  // Add 10 days to the current date
+    dueDate.setMinutes(dueDate.getMinutes() + 3);  // Add 10 days to the current date
+    // dueDate.setMinutes(dueDate.getMinutes() + 10 * 24 * 60);  // Add 10 days to the current date
 
 
     const bioBankPath = `pfw/${bioBankId}`;
@@ -4257,8 +4257,8 @@ function submitFollowup() {
 
   const timePFW = new Date()
   console.log("time", timePFW)
-  timePFW.setMinutes(timePFW.getMinutes() + 10 * 24 * 60);
-  // timePFW.setMinutes(timePFW.getMinutes() + 3);
+  // timePFW.setMinutes(timePFW.getMinutes() + 10 * 24 * 60);
+  timePFW.setMinutes(timePFW.getMinutes() + 3);
 
   const selectedStatus = document.querySelector('input[name="livestatus"]:checked').value;
   const lastfollow = document.querySelector('input[name="flexRadioDefault"]:checked').value;
